323eb5a3293afc6386207f9b5a86cd93490f7e46
Tommy Chien <ad@tommychien.com>
2020-08-26 14:07:42 +0800

    add music

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ app = Flask(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--p', type=int, default=6787)
+parser.add_argument('--duck', type=int, default=6788)
 args = parser.parse_args()
 
 addr = Dict("data/regions")
-ling = Duckling()
+music = Dict("data/music")
 engine = NluEngine(module_dir + '/data/textsmart/data/nlu/kb/', 1)
-subprocess.Popen('./data/duckling')
+subprocess.Popen(['./data/duckling', f'--port={args.duck}'])
+ling = Duckling(url=f'http://127.0.0.1:{args.duck}/parse')
 
 class decode_thread(threading.Thread):
     def __init__(self, task, text):
@@ -36,51 +38,32 @@ class decode_thread(threading.Thread):
         self.res = []
 
     def run(self):
-        # now = time.time()
         if self.task == 'general':
-            output = ling(self.text)
-            for e in output:
-                self.res.append(
-                    {
-                        'text': e['body'],
-                        'start': e['start'],
-                        'end': e['end'],
-                        'type': e['dim'],
-                        'value': e['value']
-                    }
-                )
+            self.res = process_general(ling(self.text))
         elif self.task == 'addr':
             self.res = process_addr(process_match_res(self.text, addr), self.text)
+        elif self.task == 'music':
+            self.res = process_music(self.text, music)
         elif self.task == 'ner':
-            output = engine.parse_text(self.text)
-            for entity in output.entities():
-                tmp = {'entity': entity.str, 'start': entity.offset, 'length': entity.len, 'type': entity.type.name}
-                try:
-                    tmp.update(json.loads(entity.meaning))
-                except:
-                    tmp['value'] = ""
-                self.res.append(tmp)
-        # end = time.time()
-        # print(f'{self.task}: {end-now}')
+            self.res = process_smart(engine.parse_text(self.text))
 
 @app.route('/api')
 def get():
     text = request.args.get('text', '')
     names = locals()
     threads = []
-    for task in ['general', 'addr', 'ner']:
+    for task in ['general', 'addr', 'ner', 'music']:
         names['tagger%s' % task] = decode_thread(task, text)
         threads.append(names['tagger%s' % task])
-
-    result = []
-
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
-    for thread in threads:
-        result.append(thread.res)
 
+    result = []
+    for thread in threads:
+        result.extend(thread.res)
+    post_process(result)
     return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json; charset=utf-8')
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import jieba 
+import jieba.posseg as pseg
+import json
 
 def process_match_res(text, d):
     dres = d.multi_match(text)
@@ -9,13 +11,91 @@ def process_match_res(text, d):
         tmp['start'] = dres[k]['hits'][0]['start']
         tmp['end'] = dres[k]['hits'][0]['end']
         tmp['type'] = list(dres[k]['value'].keys())[0]
-        tmp['properties'] = dres[k]['value'][tmp['type']]
+        tmp['properties'] = dres[k]['value'][tmp['type']]['type']
         res.append(tmp)
+    return res
+
+def process_music(text, d):
+    dres = d.multi_max_match(text)
+    pos = []
+    for word, flag in pseg.cut(text):
+        pos += [flag] * len(word)
+    for k,v in list(dres.items()):
+        token_pos = list(set(pos[v['hits'][0]['start']:v['hits'][0]['end']]))
+        if len(token_pos) == 1 and token_pos[0].startswith(('u', 'm')):
+            del dres[k]
+    res = []
+    for k in dres:
+        tmp = {}
+        tmp['text'] = k
+        tmp['start'] = dres[k]['hits'][0]['start']
+        tmp['end'] = dres[k]['hits'][0]['end']
+        tmp['type'] = [l for l in list(dres[k]['value'].keys()) if l != 'potential_type'][0]
+        tmp['properties'] = dres[k]['value'][tmp['type']]
+        if 'potential_type' in dres[k]['value']:
+            tmp['properties']['potential_type'] = dres[k]['value']['potential_type']
+        res.append(tmp)
+
     return res
 
 
 def process_addr(res, query):
     tokenized = list(jieba.tokenize(query))
     boundaries = {k[1]:k[2] for k in tokenized if k[2] > k[1] + 1}
-    res = [k for k in res if k['start'] in boundaries and len(k['text']) > 1]
+    for i in range(len(res) - 1, -1, -1):
+        if res[i]['start'] not in boundaries:
+            del res[i]
+        elif len(res[i]['text']) <= 1:
+            del res[i]
+        elif res[i]['properties'][0][0] == 'village' and len(res[i]['text']) == 2:
+            del res[i]
+    for i in range(len(res)):
+        res[i]['type'] = res[i]['properties'][0][0]
+        if len(res[i]['properties']) > 1:
+            res[i]['properties'] = {
+                'code': res[i]['properties'][0][1],
+                'potential_type': res[i]['properties'][1][0],
+                'potential_code': res[i]['properties'][1][1]
+                }
+        else:
+            res[i]['properties'] = {
+                'code': res[i]['properties'][0][1]
+            }
     return res
+
+def process_smart(output):
+    res = []
+    for entity in output.entities():
+        properties = {}
+        if entity.type.name.startswith(('loc.admin', 'time', 'number', 'quantity')):
+            continue
+        tmp = {
+            'text': entity.str, 
+            'start': entity.offset, 
+            'end': entity.offset + entity.len, 
+            'type': entity.type.name.split('.')[0]}
+        try:
+            properties = json.loads(entity.meaning)
+        except:
+            tmp['properties'] = ""
+        if 'related' in properties:
+            tmp['properties'] = properties['related'][:3]
+        elif 'value' in properties:
+            tmp['properties'] = properties['value']
+        res.append(tmp)
+    return res
+
+def process_general(output):
+    res = []
+    for e in output:
+        res.append({
+                'text': e['body'],
+                'start': e['start'],
+                'end': e['end'],
+                'type': e['dim'],
+                'properties': e['value']})
+    return res
+
+def post_process(result):
+    result.sort(key=lambda k: (int(k.get('start', 0))))
+    result = [e for e in result if len(e['text']) > 1]
